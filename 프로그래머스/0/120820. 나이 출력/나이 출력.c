d045b0c1fe5573fc0baee2843363179d0aa4ef9f
Ho-jin98 <hohoj98@naver.com>
2026-03-09 09:43:09 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// int solution(int age) {
+//     int answer = 0;
+//     return answer;
+// }
+
+int age = 40;
+
+int solution(int age){
+    int year = 2022;
+    int result = year - age +1;
+    return result;
+}
