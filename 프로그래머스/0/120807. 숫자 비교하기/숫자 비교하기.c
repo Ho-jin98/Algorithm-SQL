be6a2acc090971e19b65db208a3c0d36c2ce2851
Ho-jin98 <hohoj98@naver.com>
2026-03-09 09:48:24 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// int solution(int num1, int num2) {
+//     int answer = 0;
+//     return answer;
+// }
+
+int solution(int num1, int num2){
+    if (num1 == num2){
+        return 1;
+    }
+    if (num1 != num2){
+        return -1;
+    }
+}
