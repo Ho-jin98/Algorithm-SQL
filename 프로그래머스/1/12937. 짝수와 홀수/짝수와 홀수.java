26f13172306c3622334857e90e969372a5f0fccc
Ho-jin98 <hohoj98@naver.com>
2026-03-19 09:14:39 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -2,12 +2,10 @@ class Solution {
     public String solution(int num) {
         String str1 = "Even";
         String str2 = "Odd";
-        if(num % 2 == 0) {
+        if (num % 2 == 0) {
             return str1;
-        }
-        if(num % 2 != 0) {
+        } else {
             return str2;
         }
-        return "";
     }
 }
