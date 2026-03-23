109cc218313794b7a6350d178413d4f97fced7be
Ho-jin98 <hohoj98@naver.com>
2026-03-23 09:27:29 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,8 +2,12 @@ class Solution {
     public boolean solution(int x) {
         boolean result = true;
         int sum = 0;
+        // x의 자릿수를 각각 꺼내서 더해야되니, String타입으로 형변환
         String str = String.valueOf(x);
+        // 자릿수가 달라질 수 있으므로, str.length()를 활용
         for (int i = 0; i < str.length(); i++) {
+            // chatAt(i) -> 문자열에서 하나씩 꺼냄,
+            // Character.getNumericValue(); -> char타입을 숫자타입으로 변환해주는 녀석
             sum += Character.getNumericValue(str.charAt(i));
         }
         if (x % sum == 0) {return result;}
