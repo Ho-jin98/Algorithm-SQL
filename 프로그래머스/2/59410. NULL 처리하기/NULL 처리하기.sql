3ac632c8e5f2f7f1009471d09c26c49b58d62711
Ho-jin98 <hohoj98@naver.com>
2026-03-31 09:19:30 +0900

    Update NULL 처리하기.sql

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -2,3 +2,6 @@
 SELECT a.ANIMAL_TYPE, COALESCE(a.NAME, 'No name') as name, a.SEX_UPON_INTAKE
 FROM ANIMAL_INS a
 ORDER BY a.ANIMAL_ID asc;
+
+// COALESCE(컬럼, 대체값) -> 지정한 컬럼이 Null값일 때, 대체값으로 이름을 바꿔줌
+// 단, Null일 때만 대체값으로 바꿔주고, Null이 아닐 때는 원래값 그대로
