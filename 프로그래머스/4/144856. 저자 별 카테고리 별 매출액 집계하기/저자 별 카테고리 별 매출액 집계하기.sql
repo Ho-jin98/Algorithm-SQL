ca1252796a2c5f1c51e9995cd5cea97fbf1d96ac
Ho-jin98 <hohoj98@naver.com>
2026-06-01 10:04:40 +0900

    Update 저자 별 카테고리 별 매출액 집계하기.sql

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -16,3 +16,13 @@ JOIN(
 ) AS bs ON bs.BOOK_ID = b.BOOK_ID
 GROUP BY b.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
 ORDER BY b.AUTHOR_ID ASC, b.CATEGORY DESC;
+
+
+# FROM절 서브쿼리를 통해 BOOK_SALES 테이블에서 총 판매량을 먼저 조회해옴,
+# 메인쿼리에서는 서브쿼리로 먼저 조회해오는 BOOK_SALES의 총 판매량 (TOTAL_BOOK_SALES)을
+# BOOK 테이블의 PRICE와 SUM함수로 총 매출액을 조회
+
+# 주의사항
+# 메인쿼리에서 GROUP BY는 SELECT절과 컬럼 일치하는지 확인하기
+# 서브쿼리에서 SELECT절에는 총 컬럼이 2개지만, GROUP BY에서 BOOK_ID만 묶은 이유는,
+# GROUP BY는 SUM함수 같은 집계함수에 감싸여져 있지 않은 '순수 컬럼'은 반드시 GROUP BY에 명시해줘야 하기 때문이다.
